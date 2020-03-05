49b2df005712e8aa3c85f1780d4f5bfe661c6767
redbearder <redbearder@gmail.com>
2020-03-05 14:42:33 +0800

    catch exception

--- main.py
+++ main.py
@@ -17,8 +17,8 @@ from lxml import etree
 
 r = redis.Redis(host="localhost", port=6379, db=0)
 engine = create_engine(
-    "mysql+pymysql://root@localhost/wechat_puppet_crawler?charset=utf8mb4", echo=True
-)
+    "mysql+pymysql://root@localhost/wechat_puppet_crawler?charset=utf8mb4",
+    echo=True)
 Session = sessionmaker(bind=engine)
 
 es = Elasticsearch()
@@ -75,7 +75,9 @@ class Article(Base):
     account = Column(String(250))
     author = Column(String(250))
     description = Column(String(250))
-    createdAt = Column(DateTime, default=func.now(), comment="user create datetime")
+    createdAt = Column(DateTime,
+                       default=func.now(),
+                       comment="user create datetime")
     updatedAt = Column(
         DateTime,
         default=func.now(),
@@ -86,104 +88,107 @@ class Article(Base):
 
 def crawler():
     while True:
-        l = r.lpop("link")
-        if not l:
-            time.sleep(3)
-            continue
-        # crawl l
-        o = urlparse(l)
-        docid = o.path[3:]
-        all_exist_article_list = (
-            session.query(Article).filter(Article.articleid == docid).all()
-        )
-        if len(all_exist_article_list) > 0:
-            continue
-        res = requests.get(l)
-        content = res.text
+        try:
+            l = r.lpop("link")
+            if not l:
+                time.sleep(3)
+                continue
+            # crawl l
+            o = urlparse(l)
+            docid = o.path[3:]
+            all_exist_article_list = (session.query(Article).filter(
+                Article.articleid == docid).all())
+            if len(all_exist_article_list) > 0:
+                continue
+            res = requests.get(l)
+            content = res.text
 
-        soup = BeautifulSoup(content, "html.parser")
-        if len(soup.select("iframe")) > 0:
-            browserdriver.get(l.decode())
-            # print(browserdriver.page_source.encode("utf-8"))
-            html = browserdriver.find_element_by_tag_name("html")
-            time.sleep(1)
-            last_scrollY = None
-            scrollHeight = browserdriver.execute_script(
-                "return document.body.scrollHeight;"
-            )
-            while True:
-                html.send_keys(Keys.PAGE_DOWN)
-                time.sleep(1)
-                scrollY = browserdriver.execute_script("return window.scrollY")
-                if scrollY == last_scrollY:
-                    break
-                last_scrollY = scrollY
-
-            print("Reached to the bottom of the page")
-            content = browserdriver.page_source
-            content = content.replace("&amp;tp=webp", "")
             soup = BeautifulSoup(content, "html.parser")
-            iframes = soup.select("iframe")
-            for iframe in iframes:
-                if iframe.get("src"):
-                    iframe.attrs["src"] = f"https:{iframe.attrs['src']}"
-            pass
-        else:
-            soup.title.string = soup.select_one('meta[property="og:title"]').get(
-                "content"
+            if len(soup.select("iframe")) > 0:
+                browserdriver.get(l.decode())
+                # print(browserdriver.page_source.encode("utf-8"))
+                html = browserdriver.find_element_by_tag_name("html")
+                time.sleep(1)
+                last_scrollY = None
+                scrollHeight = browserdriver.execute_script(
+                    "return document.body.scrollHeight;")
+                while True:
+                    html.send_keys(Keys.PAGE_DOWN)
+                    time.sleep(1)
+                    scrollY = browserdriver.execute_script(
+                        "return window.scrollY")
+                    if scrollY == last_scrollY:
+                        break
+                    last_scrollY = scrollY
+
+                print("Reached to the bottom of the page")
+                content = browserdriver.page_source
+                content = content.replace("&amp;tp=webp", "")
+                soup = BeautifulSoup(content, "html.parser")
+                iframes = soup.select("iframe")
+                for iframe in iframes:
+                    if iframe.get("src"):
+                        iframe.attrs["src"] = f"https:{iframe.attrs['src']}"
+                pass
+            else:
+                soup.title.string = soup.select_one(
+                    'meta[property="og:title"]').get("content")
+                imglist = soup.select("img")
+                for img_element in imglist:
+                    if img_element.get("data-src"):
+                        img_element.attrs["src"] = img_element.attrs[
+                            "data-src"]
+                pass
+
+            # xpath
+            # selector = etree.HTML(content)
+            # links = selector.xpath('//h4/a/text()')
+            article_title = soup.select_one('meta[property="og:title"]').get(
+                "content")
+            article_description = soup.select_one(
+                'meta[property="og:description"]').get("content")
+            article_author = soup.select_one(
+                'meta[property="og:article:author"]').get("content")
+            article_url = soup.select_one('meta[property="og:url"]').get(
+                "content")
+            article_image = soup.select_one('meta[property="og:image"]').get(
+                "content")
+            article_account = soup.select_one(".profile_nickname").string
+
+            script_list = soup.select("script")
+            for script_element in script_list:
+                script_element.decompose()
+
+            article_content = soup.prettify()
+
+            es.index(
+                index="article",
+                id=docid,
+                body={
+                    "title": article_title,
+                    "articleid": docid,
+                    "description": article_description,
+                    "author": article_author,
+                    "url": article_url,
+                    "image": article_image,
+                    "account": article_account,
+                    "content": article_content,
+                },
             )
-            imglist = soup.select("img")
-            for img_element in imglist:
-                if img_element.get("data-src"):
-                    img_element.attrs["src"] = img_element.attrs["data-src"]
-            pass
 
-        # xpath
-        # selector = etree.HTML(content)
-        # links = selector.xpath('//h4/a/text()')
-        article_title = soup.select_one('meta[property="og:title"]').get("content")
-        article_description = soup.select_one('meta[property="og:description"]').get(
-            "content"
-        )
-        article_author = soup.select_one('meta[property="og:article:author"]').get(
-            "content"
-        )
-        article_url = soup.select_one('meta[property="og:url"]').get("content")
-        article_image = soup.select_one('meta[property="og:image"]').get("content")
-        article_account = soup.select_one(".profile_nickname").string
-
-        script_list = soup.select("script")
-        for script_element in script_list:
-            script_element.decompose()
-
-        article_content = soup.prettify()
-
-        es.index(
-            index="article",
-            id=docid,
-            body={
-                "title": article_title,
-                "articleid": docid,
-                "description": article_description,
-                "author": article_author,
-                "url": article_url,
-                "image": article_image,
-                "account": article_account,
-                "content": article_content,
-            },
-        )
-
-        new_a = Article(
-            title=article_title,
-            articleid=docid,
-            url=article_url,
-            image=article_image,
-            account=article_account,
-            author=article_author,
-            description=article_description,
-        )
-        session.add(new_a)
-        session.commit()
+            new_a = Article(
+                title=article_title,
+                articleid=docid,
+                url=article_url,
+                image=article_image,
+                account=article_account,
+                author=article_author,
+                description=article_description,
+            )
+            session.add(new_a)
+            session.commit()
+        except:
+            continue
     pass
 
 
@@ -192,7 +197,8 @@ if __name__ == "__main__":
 
     while True:
         try:
-            driver = webdriver.Remote("http://localhost:4723/wd/hub", desired_caps)
+            driver = webdriver.Remote("http://localhost:4723/wd/hub",
+                                      desired_caps)
             chatwlist = []
             while len(chatwlist) == 0:
                 chatwlist = driver.find_elements_by_id(chatlistid)
@@ -208,17 +214,18 @@ if __name__ == "__main__":
                     # article_element_list = driver.find_elements_by_id(
                     # 'com.tencent.mm:id/a9n')
                     article_element_list = driver.find_elements_by_xpath(
-                        '//android.widget.ImageView[@content-desc="图片"]'
-                    )
+                        '//android.widget.ImageView[@content-desc="图片"]')
                     if len(article_element_list) == 0:
                         break
                     for e in article_element_list:
                         e.click()
                         time.sleep(10)
-                        opbtn = driver.find_element_by_id("com.tencent.mm:id/l0")
+                        opbtn = driver.find_element_by_id(
+                            "com.tencent.mm:id/l0")
                         opbtn.click()
                         time.sleep(3)
-                        copybtns = driver.find_elements_by_id("com.tencent.mm:id/d0")
+                        copybtns = driver.find_elements_by_id(
+                            "com.tencent.mm:id/d0")
                         copybtnidx = 0
                         for btn in copybtns:
                             if btn.text == "复制链接":
@@ -229,9 +236,11 @@ if __name__ == "__main__":
                             copybtnidx += 1
 
                         if copybtnidx == 0:
-                            backbtn = driver.find_element_by_id("com.tencent.mm:id/lc")
+                            backbtn = driver.find_element_by_id(
+                                "com.tencent.mm:id/lc")
                             backbtn.click()
-                        backbtn = driver.find_element_by_id("com.tencent.mm:id/lc")
+                        backbtn = driver.find_element_by_id(
+                            "com.tencent.mm:id/lc")
                         backbtn.click()
                         time.sleep(3)
                         pass
